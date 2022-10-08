7171c6f7d2622f668a7fa54728f03aa60907cd29
Luke Leedy <lukeleedy123@gmail.com>
2022-10-08 18:23:09 -0400

    Hide bars with no data

--- EnergyVisualization/Assets/Building.cs
+++ EnergyVisualization/Assets/Building.cs
@@ -6,11 +6,13 @@ public class Building : MonoBehaviour
 
     private Transform building;
     private Material material;
+    private MeshRenderer meshRenderer;
 
     private void Awake()
     {
         building = transform;
-        material = GetComponent<MeshRenderer>().material;
+        meshRenderer = GetComponent<MeshRenderer>();
+        material = meshRenderer.material;
     }
 
     private void Start()
@@ -21,11 +23,16 @@ public class Building : MonoBehaviour
     public void Adjust()
     {
         DataManager dm = DataManager.Instance;
+        BuildingManager bm = BuildingManager.Instance;
+
         float data = dm.GetData(_name);
-        float height = BuildingManager.Instance.MaxBuildingHeight * dm.GetRatio(data);
+        float height = bm.MaxBuildingHeight * dm.GetRatio(data);
 
         material.color = dm.GetColorFromData(data);
         building.localScale = new Vector3(building.localScale.x, height, building.localScale.z);
         building.position = new Vector3(building.position.x, height / 2, building.position.z);
+
+        // Hide buildings with data 0 or they will produce visual artifacts
+        meshRenderer.enabled = !Mathf.Approximately(0, data);
     }
 }
