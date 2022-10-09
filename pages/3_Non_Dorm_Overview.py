05aa77bf215418256a7b28e3ad7a27c3d38c20ee
Jenny Yi <yi.404@osu.edu>
2022-10-08 21:51:30 -0400

    Add total num

--- pages/3_Non_Dorm_Overview.py
+++ pages/3_Non_Dorm_Overview.py
@@ -16,7 +16,29 @@ def overall_change_year():
     for col in data.columns:
         data.rename(columns={col:col.replace(" - Total Energy Consumption (Cleaned) (kBTU)","")},inplace=True)
     st.subheader('Total Energy Consumption by Building for Each Year in kBTU')
+    st.line_chart(data=data)
+
+def overall_change_year_type():
+    data = annual_data.filter(regex='Total Energy')
+    data = data.drop(data.index[0])
+    for col in data.columns:
+        data.rename(columns={col:col.replace(" - Total Energy Consumption (Cleaned) (kBTU)","")},inplace=True)
     st.bar_chart(data=data)
+
+def total_average_energy():
+    data = annual_data.filter(regex='Total Energy')
+    data = data.filter(regex='Mean', axis=0)
+    data['Energy Consumption (kbTU)'] = data.filter(like='Total').sum(1)
+    data = data[data.columns.drop(list(data.filter(regex='Total Energy')))]
+    st.metric(label="Average Total Energy Consumption per Year (kbTU)", value=round(data['Energy Consumption (kbTU)'], 2))
+
+def overall_change_year():
+    data = annual_data.filter(regex='Total Energy')
+    data = data.drop(data.index[0])
+    for col in data.columns:
+        data.rename(columns={col:col.replace(" - Total Energy Consumption (Cleaned) (kBTU)","")},inplace=True)
+    st.subheader('Total Energy Consumption by Building for Each Year in kBTU')
+    st.line_chart(data=data)
 
 def average_total_building():
     data = annual_data.filter(regex='Total Energy')
@@ -35,13 +57,15 @@ def average_total_type():
     data['Hot Water Consumption (kbTU)'] = data.filter(like='Hot').sum(1)
     data['Natural Gas Consumption (kbTU)'] = data.filter(like='Natural').sum(1)
     data = data[data.columns.drop(list(data.filter(regex=' - ')))]
-    st.subheader('Percentages of Average Total Energy Consumption by Type')
+    st.subheader('Percentages of Average Yearly Total Energy Consumption by Type')
     fig = px.pie(values=data.iloc[0], names=data.columns)
     st.plotly_chart(fig)
 
 
 
 overall_change_year()
+overall_change_year_type()
+total_average_energy()
 average_total_building()
 average_total_type()
 
