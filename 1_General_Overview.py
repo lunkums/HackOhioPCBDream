14cdeaf417b4406b893bd39402b2205cf02828db
Grace-coder311 <gworkman49@gmail.com>
2022-10-09 01:47:48 -0400

    made things pretty in general asdjfsaoidfj

--- 1_General_Overview.py
+++ 1_General_Overview.py
@@ -10,9 +10,8 @@ import plotly.express as px
 
 st.write("# Welcome to OSU Campus Energy Dashboard! 💃")
 
-st.sidebar.success("Select a dashboard above.")
-
-df = pd.read_csv('data/Non-Dorm Annual Means per Person.csv', index_col='StatType')
+df = pd.read_csv('data/Non-Dorm Annual Means per Person.csv',
+                 index_col='StatType')
 
 # mean_rows = df.filter(regex="^Mean", axis=0)  # select rows
 # mean_gas = mean_rows.filter(regex='Natural Gas Consumption', axis=1)  # select columns
@@ -44,6 +43,7 @@ annual_Dorm_data = pd.read_csv(
 #TODO need to include non-dorm and dorm data together!
 #print(combined_data)
 
+
 def combine_all_buildings():
     left = annual_Dorm_data.filter(regex='Total Energy', axis=1)
     left = left.drop(['Mean (2017-2022)'])
@@ -54,41 +54,63 @@ def combine_all_buildings():
 
     data = left.merge(right, left_index=True, right_index=True, how='inner')
     #st.dataframe(data)
- 
+
     for col in data.columns:
-         data.rename(columns={col: col.replace(" - Total Energy Consumption (Cleaned) (kBTU)", "")}, inplace=True)
+        data.rename(columns={col: col.replace(
+            " - Total Energy Consumption (Cleaned) (kBTU)", "")}, inplace=True)
 
     return data
+
 
 def combined_change_year():
     data = combine_all_buildings()
     st.dataframe(data)
     building_names = data.columns
+    all_years_list = list(data.index)
+    #st.sidebar.write("Barchart Filter")
+    #form = st.form(key='bcf')
+    
+    with st.sidebar:
+        with st.expander("Barchart Filter"):
+            c = st.container()   
+            all_options = c.checkbox("Select all buildings", value=True)
+            if all_options:
+                select_buildings = data.columns
+                st.snow()
+            else:
+                select_buildings = c.multiselect(
+                    "Select Buildings to Compare",
+                    building_names,
+                    default=building_names[0]
+                )
+            
 
-    all_options = st.checkbox("Select all buildings", value=True)
+            all_years = c.checkbox("Select Years", value=True)
+            select_years = all_years_list
+            if not all_years:
+                select_years = c.multiselect(
+                    "Select Buildings to Compare",
+                    select_years,
+                    default=select_years[0]
+                )
 
-    if all_options:
-        select_buildings = data.columns
-    else:
-        select_buildings = st.multiselect(
-            "Select Buildings to Compare",
-            building_names,
-            default = building_names[0]
-        )
 
     st.subheader('Total Energy Consumption by Building for Each Year in kBTU')
-    st.bar_chart(data=data[select_buildings])
+    st.bar_chart(data=data.loc[select_years, select_buildings])
+
 
 def combined_average_total_building():
-    data = pd.concat([annual_Nondorm_data, annual_Dorm_data], axis=0, ignore_index=False)
-    data = data.filter(regex='^Mean\s\D\d*\D\d*$', axis=0) 
+    data = pd.concat([annual_Nondorm_data, annual_Dorm_data],
+                     axis=0, ignore_index=False)
+    data = data.filter(regex='^Mean\s\D\d*\D\d*$', axis=0)
     data = data.filter(regex='Total Energy')
-    
-    data =  combine_all_buildings()
+
+    data = combine_all_buildings()
 
     fig = px.pie(values=data.iloc[0], names=data.columns)
     st.subheader('Average Total Energy Consumption by Building in kBTU')
     st.plotly_chart(fig)
+
 
 combined_change_year()
 combined_average_total_building()
