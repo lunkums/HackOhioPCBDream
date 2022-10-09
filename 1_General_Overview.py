b616821f283faa3c352eb6fa667ce29c53f51a73
Grace-coder311 <gworkman49@gmail.com>
2022-10-08 20:52:43 -0400

    select certain buildings for Non Dorm overview bar chart @Jenny

--- 1_General_Overview.py
+++ 1_General_Overview.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
+import plotly.express as px
 
 # st.set_page_config(
 #     page_title="Hello",
@@ -12,34 +14,49 @@ st.sidebar.success("Select a dashboard above.")
 
 df = pd.read_csv('data/Annual Means per Person.csv', index_col = 'StatType')
 
-
-
-mean_rows = df.filter(regex="^Mean", axis=0)  # select rows
-mean_gas = mean_rows.filter(regex='Natural Gas Consumption', axis=1)  # select columns
-mean_electric = mean_rows.filter(regex='Electricity Consumption', axis=1)
-gasSum = mean_gas.to_numpy(na_value=0).flatten().sum()
-electricSum = mean_electric.to_numpy(na_value=0).flatten().sum()
-totalSum = gasSum + electricSum
-print(totalSum)
-print(gasSum)
-print(electricSum)
-gasP = gasSum/totalSum * 100
-elecP = electricSum/totalSum * 100
-#print(gasSum/totalSum *100)
-# df = pd.DataFrame({'mass': [0.330, 4.87 , 5.97],
-#                    'radius': [2439.7, 6051.8, 6378.1]},
-#                   index=['Mercury', 'Venus', 'Earth'])
-# plot = df.plot.pie(y='mass', figsize=(5, 5))
+# mean_rows = df.filter(regex="^Mean", axis=0)  # select rows
+# mean_gas = mean_rows.filter(regex='Natural Gas Consumption', axis=1)  # select columns
+# mean_electric = mean_rows.filter(regex='Electricity Consumption', axis=1)
+# gasSum = mean_gas.to_numpy(na_value=0).flatten().sum()
+# electricSum = mean_electric.to_numpy(na_value=0).flatten().sum()
+# totalSum = gasSum + electricSum
+# print(totalSum)
+# print(gasSum)
+# print(electricSum)
+# gasP = gasSum/totalSum * 100
+# elecP = electricSum/totalSum * 100
+# #print(gasSum/totalSum *100)
+# # df = pd.DataFrame({'mass': [0.330, 4.87 , 5.97],
+# #                    'radius': [2439.7, 6051.8, 6378.1]},
+# #                   index=['Mercury', 'Venus', 'Earth'])
+# # plot = df.plot.pie(y='mass', figsize=(5, 5))
+# # st.pyplot(plot.figure)
+# cDF = pd.DataFrame({'Percents': [gasP, elecP]}, index = ['Natural Gas', 'Electric'])
+# plot = cDF.plot.pie(y='Percents', figsize=(3,3), radius = 0.9)
 # st.pyplot(plot.figure)
-cDF = pd.DataFrame({'Percents': [gasP, elecP]}, index = ['Natural Gas', 'Electric'])
-plot = cDF.plot.pie(y='Percents', figsize=(3,3), radius = 0.9)
-st.pyplot(plot.figure)
 
-#now compare percent of electric to gas annual means per person
+annual_data = pd.read_csv('data/Non-Dorm Annual Basic Stats.csv', index_col='Stat Type')
 
-#print(mean_electric.sum())
-#plot = df.plot.pie(y='Energy consumed', figsize=(5, 5))
-#print(mean_rows)#
+#TODO need to include non-dorm and dorm data together!
+def overall_change_year():
+    data = annual_data.filter(regex='Total Energy')
+    data = data.drop(data.index[0])
+    for col in data.columns:
+        data.rename(columns={col:col.replace(" - Total Energy Consumption (Cleaned) (kBTU)","")},inplace=True)
+    
+    building_names = data.columns
+    select_buildings = st.multiselect(
+        "Select Buildings to Compare",
+        building_names,
+        default= building_names[0]
+    )
+
+    st.subheader('Total Energy Consumption by Building for Each Year in kBTU')
+    st.bar_chart(data=data[select_buildings])
+   
+
+overall_change_year()
+
 
 # st.markdown(
 #     """
